1f02a3a4f220e3d471d2c420dde33d0894d94f19
Adam <adam.sasine@gmail.com>
2017-01-28 20:12:23 -0500

    Added License header to all Java files Removed author tags

--- app/src/main/java/com/teamltt/carcare/adapter/bluetooth/DeviceSocket.java
+++ app/src/main/java/com/teamltt/carcare/adapter/bluetooth/DeviceSocket.java
@@ -1,3 +1,19 @@
+/*
+ ** Copyright 2017, Team LTT
+ **
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
+
 package com.teamltt.carcare.adapter.bluetooth;
 
 import android.bluetooth.BluetoothSocket;
@@ -7,10 +23,6 @@ import com.teamltt.carcare.adapter.IObdSocket;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
-/**
- * Created by Jack on 1/25/2017.
- */
 
 public class DeviceSocket implements IObdSocket {
     private BluetoothSocket socket;
