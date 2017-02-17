7071e777a86c82cdc06a47f49806d207e4dbb64f
Adam <adam.sasine@gmail.com>
2017-02-17 17:57:02 -0500

    Changed user_id to autoincrementing

--- app/src/main/java/com/teamltt/carcare/database/DbHelper.java
+++ app/src/main/java/com/teamltt/carcare/database/DbHelper.java
@@ -49,7 +49,7 @@ public class DbHelper extends SQLiteOpenHelper implements IObservable {
     public static final long DB_WRITE_ERROR = -1; // from SQLiteDatabase if an error occurred
     public static final long DB_OK = 0;
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "CarCare.db";
 
     private static final SimpleDateFormat sqlDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
