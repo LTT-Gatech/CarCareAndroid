c6a4b67287e2e542e93439e72821de23e64cb024
giulls <giulianolombardi@mac.com>
2017-02-19 16:09:24 -0500

    Remove menu from car edit info

--- app/src/main/java/com/teamltt/carcare/activity/CarInfoEditActivity.java
+++ app/src/main/java/com/teamltt/carcare/activity/CarInfoEditActivity.java
@@ -84,37 +84,6 @@ public class CarInfoEditActivity extends AppCompatActivity {
         super.onDestroy();
         dbHelper.close();
     }
-<<<<<<< HEAD
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent;
-        switch (item.getItemId()) {
-            case (R.id.action_carInfo):
-                intent = new Intent(this, CarInfoActivity.class);
-                startActivity(intent);
-                break;
-            case (R.id.action_demo):
-                intent = new Intent(this, DemoActivity.class);
-                startActivity(intent);
-                break;
-            case (R.id.action_trips):
-                intent = new Intent(this, TripsActivity.class);
-                startActivity(intent);
-            case (R.id.action_dynamic):
-                intent = new Intent(this, DynamicActivity.class);
-                startActivity(intent);
-                break;
-            case (R.id.action_reminder):
-                intent = new Intent(this, ReminderActivity.class);
-                startActivity(intent);
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
-    public void toggleLogging(MenuItem item) {
-        //
-    }
-=======
->>>>>>> origin/simulator
 }
